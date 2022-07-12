4e325a1e0ef871773920e9e614de6146c4742e59
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-07-12 13:37:44 -0300

    ajustes lint

--- tests/getUser.test.js
+++ tests/getUser.test.js
@@ -1,9 +1,9 @@
+/* eslint-disable mocha/no-hooks-for-single-case */
 const supertest = require('supertest');
 const app = require('../src/api/app');
 const { User } = require('../src/models');
 
-
-describe('Resposta do endpoint GET /users', () => {
+describe('Resposta do endpoint GET /users', function () {
     let response;
 
     const findAllMock = [{
@@ -11,19 +11,18 @@ describe('Resposta do endpoint GET /users', () => {
         firstName: 'John',
         lastName: 'Doe',
         email: 'example@example.com',
-        createdAt: "2021-10-21T18:02:44.106Z",
-        updatedAt: "2021-10-21T18:02:44.106Z",
-    }]
+        createdAt: '2021-10-21T18:02:44.106Z',
+        updatedAt: '2021-10-21T18:02:44.106Z',
+    }];
 
-    beforeAll(async () => {
+    beforeAll(async function () {
         jest.spyOn(User, 'findAll').mockImplementation(() => findAllMock);
 
         response = await supertest(app)
             .get('/users');
     });
 
-    it('deve retornar um array de objetos idêntico ao usado no Stub', () => {
+    it('deve retornar um array de objetos idêntico ao usado no Stub', function () {
         expect(response.body).toEqual(findAllMock);
     });
-
-})
+});
